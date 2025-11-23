b192daeca98684cac31b88e94d64af9594d56a28
Mariana <mariana.iwaivk@gmail.com>
2025-11-23 05:58:24 -0300

    feat(Home): add Home page component

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import BannerSection from "../../components/BannerSection";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import WorkExperienceSection from "../../components/WorkExperienceSection";
+
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <BannerSection />
+            <WorkExperienceSection />
+            <Footer />
+        </>
+    );
+}
