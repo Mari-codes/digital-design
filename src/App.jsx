eddb23ddf8905a04e9e7b735d7a722be2fd94ef2
Mariana <mariana.iwaivk@gmail.com>
2025-11-23 06:00:46 -0300

    feat(App): render Home page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Header from './components/Header'
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
-    <Header />
+    <Home/>
     </>
   )
 }
